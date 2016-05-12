4908dd59842f32ce5e8cbf6b8d35ce1bee05e675
seitanas <tadas@ring.lt>
2016-05-12 08:29:39 +0300

    Fixes issue #4, where machines with missing drive images cannot be deleted.

--- delete_vm.php
+++ delete_vm.php
@@ -3,7 +3,7 @@
 KVM-VDI
 Tadas Ustinavičius
 tadas at ring.lt
-2016-04-21
+2016-05-12
 Vilnius, Lithuania.
 */
 include ('functions/config.php');
@@ -22,12 +22,13 @@ $h_reply=get_SQL_line("SELECT * FROM hypervisors WHERE id='$hypervisor'");
 ssh_connect($h_reply[2].":".$h_reply[3]);
 $v_reply=get_SQL_line("SELECT name FROM vms WHERE id='$vm'");
 $source_path=str_replace("\n", "",(ssh_command("sudo virsh domblklist " . $v_reply[0] . "|grep vda| awk '{print $2}' ",true)));
+echo $source_path;
 if (!empty($source_path)){
     ssh_command("sudo virsh destroy " . $v_reply[0], true);
     ssh_command("sudo virsh undefine " . $v_reply[0], true);
     ssh_command("sudo rm " . $source_path, true);
-    add_SQL_line("DELETE FROM vms WHERE id='$vm' LIMIT 1");
 }
+add_SQL_line("DELETE FROM vms WHERE id='$vm' LIMIT 1");
 header("Location: $serviceurl/reload_vm_info.php");
 exit;
 ?>
