0afa03e1953e0cf888cd8b7bc45b9023f2669f65
seitanas <tadas@ring.lt>
2016-05-13 21:34:36 +0300

    Fixing problem with non-virtio storage controllers

--- delete_vm.php
+++ delete_vm.php
@@ -23,6 +23,11 @@ $h_reply=get_SQL_line("SELECT * FROM hypervisors WHERE id='$hypervisor'");
 ssh_connect($h_reply[2].":".$h_reply[3]);
 $v_reply=get_SQL_line("SELECT name FROM vms WHERE id='$vm'");
 $source_path=str_replace("\n", "",(ssh_command("sudo virsh domblklist " . $v_reply[0] . "|grep vda| awk '{print $2}' ",true)));
+if (empty ($source_path))
+    $source_path=str_replace("\n", "",(ssh_command("sudo virsh domblklist $v_reply[0]|grep hda| awk '{print $2}' ",true)));
+if (empty ($source_path)||$source_path=='-'||strtolower(substr($source_path, -4))=='.iso')//if we have cd drive, then disk image would be second drive
+    $source_path=str_replace("\n", "",(ssh_command("sudo virsh domblklist $v_reply[0]|grep hdb| awk '{print $2}' ",true)));
+
 echo $source_path;
 if (!empty($source_path)){
     ssh_command("sudo virsh destroy " . $v_reply[0], true);
