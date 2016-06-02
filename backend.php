3357ebee70800dc93aa2471eabba7201f6049c2a
seitanas <tadas@ring.lt>
2016-06-02 08:42:35 +0300

    Fixing some warnings

--- backend.php
+++ backend.php
@@ -6,8 +6,6 @@ $pass=$_POST['pass'];
 if ($pass==$backend_pass){
     $tmpname=$_POST['tmpname'];
     $data=$_POST['data'];
-    $vm=$_POST['vm'];
-    $spice_password=$_POST['spice_password'];
     if (!empty($tmpname)&&!empty($data)){
         file_put_contents("tmp/".$tmpname.".txt", $data);
 	if ($data==100){
@@ -15,7 +13,10 @@ if ($pass==$backend_pass){
 	    add_SQL_line("UPDATE vms SET filecopy='' WHERE filecopy='$tmpname'");
 	}
     }
+    if (isset($_POST['vm']))
+	$vm=$_POST['vm'];
     if(!empty($vm)){
+	$spice_password=$_POST['spice_password'];
 	add_SQL_line("UPDATE vms SET spice_password='$spice_password' WHERE name='$vm'");
 	$v_reply=get_SQL_line("SELECT snapshot FROM vms WHERE name='$vm'");
 	echo $v_reply[0];
