6be576d5e647fb3fb7ca18e924daf6f78b27ddb9
seitanas <tadas@ring.lt>
2016-07-26 10:00:26 +0300

    Forcing newly created VMs to detach from console

--- create_vm.php
+++ create_vm.php
@@ -52,13 +52,15 @@ if ($machine_type=='simplemachine'||$machine_type=='sourcemachine'){
 	else
     	    $name=$machinename;
 	$disk=$source_drivepath . '/' . $name . "-" . uniqid() . ".qcow2";
-	$vm_cmd="sudo virt-install --name=" . $name . " --disk path=" . $disk . ",format=qcow2,bus=virtio,cache=none --soundhw=ac97 --vcpus=" . $numcpu . ",cores=" . $numcore . " --ram=" . $numram . " --network bridge=" . $network . ",model=virtio --os-type=" . $os_type . " --os-variant=" . $os_version . " --graphics spice,listen=0.0.0.0 --redirdev usb,type=spicevmc --video qxl --noreboot " . $boot_cmd;
+	$vm_cmd="sudo virt-install --name=" . $name . " --disk path=" . $disk . ",format=qcow2,bus=virtio,cache=none --soundhw=ac97 --vcpus=" . $numcpu . ",cores=" . $numcore . " --ram=" . $numram . " --network bridge=" . $network . ",model=virtio --os-type=" . $os_type . " --os-variant=" . $os_version . " --graphics spice,listen=0.0.0.0 --redirdev usb,type=spicevmc --video qxl --noreboot --noautoconsole " . $boot_cmd;
 	$drive_cmd="sudo qemu-img create -f qcow2 -o size=" . $source_drive_size . "G " . $disk;
 	$chown_command="sudo chown $libvirt_user:$libvirt_group $disk";
+	$xmledit_cmd="sudo " . $hypervisor_cmdline_path . "/vdi-xmledit -name " . $name;
 	add_SQL_line("INSERT INTO  vms (name,hypervisor,machine_type) VALUES ('$name','$hypervisor','$machine_type')");
 	ssh_command($drive_cmd,true);
         ssh_command($chown_command,true);
 	ssh_command($vm_cmd,true);
+	ssh_command($xmledit_cmd,true);
 	++$x;
 
     }
@@ -66,12 +68,14 @@ if ($machine_type=='simplemachine'||$machine_type=='sourcemachine'){
 if ($machine_type=='initialmachine'){
     $name=$machinename;
     $disk=$source_drivepath . '/' . $name . "-" . uniqid() . ".qcow2";
-    $vm_cmd="sudo virt-install --name=" . $name . " --disk path=" . $disk . ",format=qcow2,bus=virtio,cache=none --soundhw=ac97 --vcpus=" . $numcpu . ",cores=" . $numcore . " --ram=" . $numram . " --network bridge=" . $network . ",model=virtio --os-type=" . $os_type . " --os-variant=" . $os_version . " --graphics spice,listen=0.0.0.0 --redirdev usb,type=spicevmc --video qxl --import --noreboot";
+    $vm_cmd="sudo virt-install --name=" . $name . " --disk path=" . $disk . ",format=qcow2,bus=virtio,cache=none --soundhw=ac97 --vcpus=" . $numcpu . ",cores=" . $numcore . " --ram=" . $numram . " --network bridge=" . $network . ",model=virtio --os-type=" . $os_type . " --os-variant=" . $os_version . " --graphics spice,listen=0.0.0.0 --redirdev usb,type=spicevmc --video qxl --import --noreboot --noautoconsole";
     $drive_cmd="sudo qemu-img create -f qcow2 -o size=1G " . $disk;
     $chown_command="sudo chown $libvirt_user:$libvirt_group $disk";
+    $xmledit_cmd="sudo " . $hypervisor_cmdline_path . "/vdi-xmledit -name " . $name;
     ssh_command($drive_cmd,true);
     ssh_command($chown_command,true);
     ssh_command($vm_cmd,true);
+    ssh_command($xmledit_cmd,true);
     add_SQL_line("INSERT INTO  vms (name,hypervisor,machine_type,source_volume) VALUES ('$name','$hypervisor','$machine_type','$source_volume')");
     $v_reply=get_SQL_line("SELECT id FROM vms WHERE name='$name'");
     header("Location: $serviceurl/copy_disk.php?vm=" . $v_reply[0] . "&hypervisor=" . $hypervisor);
