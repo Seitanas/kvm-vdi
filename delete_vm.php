1b0f602841ece40d009c0e56c2b86ba1b05650c7
seitanas <tadas@ring.lt>
2016-08-31 09:49:57 +0300

    Force deleting child VDI VMs if deleting initial image. May affect issue #50

--- delete_vm.php
+++ delete_vm.php
@@ -3,7 +3,7 @@
 KVM-VDI
 Tadas Ustinavičius
 tadas at ring.lt
-2016-08-25
+2016-08-31
 Vilnius, Lithuania.
 */
 include ('functions/config.php');
@@ -30,7 +30,24 @@ if (empty($vm)&&empty($parent)){
 if ($action!='mass_delete'){
     $h_reply=get_SQL_array("SELECT * FROM hypervisors WHERE id='$hypervisor'");
     ssh_connect($h_reply[0]['ip'] . ":" . $h_reply[0]['port']);
-    $v_reply=get_SQL_array("SELECT name FROM vms WHERE id='$vm'");
+    $v_reply=get_SQL_array("SELECT id,name,machine_type FROM vms WHERE id='$vm'");
+    if ($v_reply[0]['machine_type']=='initialmachine'){//we need to delete child VMs if machine is initial
+	$child_vms=get_SQL_array("SELECT id,name FROM vms WHERE source_volume='{$v_reply[0]['id']}'");
+	$x=0;
+	while ($x<sizeof($child_vms)){
+	    $source_path=str_replace("\n", "",(ssh_command("sudo virsh domblklist " . $child_vms[$x]['name'] . "|grep vda| awk '{print $2}' ",true)));
+	    if (empty ($source_path))
+    		$source_path=str_replace("\n", "",(ssh_command("sudo virsh domblklist " . $child_vms[$x]['name'] . "|grep hda| awk '{print $2}' ",true)));
+	    if (empty ($source_path)||$source_path=='-'||strtolower(substr($source_path, -4))=='.iso')//if we have cd drive, then disk image would be second drive
+		$source_path=str_replace("\n", "",(ssh_command("sudo virsh domblklist " . $child_vms[$x]['name'] . "|grep hdb| awk '{print $2}' ",true)));
+	    if (!empty($source_path))
+		write_log(ssh_command("sudo rm " . $source_path, true));
+	    write_log(ssh_command("sudo virsh destroy " . $child_vms[$x]['name'], true));
+	    write_log(ssh_command("sudo virsh undefine " . $child_vms[$x]['name'], true));
+	    add_SQL_line("DELETE FROM vms WHERE id='{$child_vms[$x]['id']}' LIMIT 1");
+	    ++$x;
+	}
+    }
     $source_path=str_replace("\n", "",(ssh_command("sudo virsh domblklist " . $v_reply[0]['name'] . "|grep vda| awk '{print $2}' ",true)));
     if (empty ($source_path))
         $source_path=str_replace("\n", "",(ssh_command("sudo virsh domblklist " . $v_reply[0]['name'] . "|grep hda| awk '{print $2}' ",true)));
