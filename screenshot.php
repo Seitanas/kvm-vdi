5d139f6b7080eba3d6420df572aa145f0c822760
Seitanas <tadas@ring.lt>
2016-09-06 13:18:16 +0300

    Temporary fix for issue #53

--- screenshot.php
+++ screenshot.php
@@ -15,6 +15,11 @@ $h_reply=get_SQL_line("SELECT * FROM hypervisors WHERE id='$hypervisor'");
 $v_reply=get_SQL_line("SELECT * FROM vms WHERE id='$vm'");
 $filepath=$temp_folder. '/' . $v_reply[1] . ".ppm";
 ssh_connect($h_reply[2].":".$h_reply[3]);
+#images with .ppmbugfix extensions are used as temporary dirty-fix for 
+#issue, where only every second screenshot is updated from libvirt/qemu.
+#https://www.redhat.com/archives/libvirt-users/2016-August/msg00075.html
+#http://lists.nongnu.org/archive/html/qemu-discuss/2016-09/msg00000.html
+ssh_command("sudo virsh screenshot " . $v_reply[1] . " " . $filepath."bugfix");
 ssh_command("sudo virsh screenshot " . $v_reply[1] . " " . $filepath, true);
 $im=ssh_command("cat ". $filepath,true);
 $image = new Imagick();
@@ -23,4 +28,5 @@ $image->setImageFormat("png");
 $image->scaleImage(865, 865, true);
 header("Content-type: image/png");
 echo $image->getImageBlob();
+ssh_command("rm " . $filepath."bugfix", true);
 ssh_command("rm " . $filepath, true);
