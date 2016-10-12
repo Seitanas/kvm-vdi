fc83ed692924e26d7ca83b883e12283574aa6af2
Seitanas <tadas@ring.lt>
2016-10-12 12:29:34 +0300

    Created Lithuanian keymap for SPICE HTML5

---
+++ spice_html5/keymaps/keymaplt.js
@@ -0,0 +1,203 @@
+wdi.KeymapLT = function() {
+
+    var charmapLT = [];
+    charmapLT['`']   = [[0x29, 0, 0, 0], [0xA9, 0, 0, 0]];
+    charmapLT['~']   = [[0x2A, 0, 0, 0], [0x29, 0, 0, 0], [0xA9, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['ą']   = [[0x2, 0, 0, 0],[0x82, 0, 0, 0]];
+    charmapLT['Ą']   = [[0x2A, 0, 0, 0], [0x2, 0, 0, 0], [0x82, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['č']   = [[0x3, 0, 0, 0], [0x83, 0, 0, 0]];
+    charmapLT['Č']   = [[0x2A, 0, 0, 0], [0x3, 0, 0, 0], [0x83, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['ę']   = [[0x4, 0, 0, 0], [0x84, 0, 0, 0]];
+    charmapLT['Ę']   = [[0x2A, 0, 0, 0], [0x4, 0, 0, 0], [0x84, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['ė']   = [[0x5, 0, 0, 0], [0x85, 0, 0, 0]];
+    charmapLT['Ė']   = [[0x2A, 0, 0, 0], [0x5, 0, 0, 0], [0x85, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['į']   = [[0x6, 0, 0, 0], [0x86, 0, 0, 0]];
+    charmapLT['Į']   = [[0x2A, 0, 0, 0], [0x6, 0, 0, 0], [0x86, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['š']   = [[0x7, 0, 0, 0], [0x87, 0, 0, 0]];
+    charmapLT['Š']   = [[0x2A, 0, 0, 0], [0x7, 0, 0, 0], [0x87, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['ų']   = [[0x8, 0, 0, 0], [0x88, 0, 0, 0]];
+    charmapLT['Ų']   = [[0x2A, 0, 0, 0], [0x8, 0, 0, 0], [0x88, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['ū']   = [[0x9, 0, 0, 0], [0x89, 0, 0, 0]];
+    charmapLT['Ū']   = [[0x2A, 0, 0, 0], [0x9, 0, 0, 0], [0x89, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['„']   = [[0x0A, 0, 0, 0], [0x8A, 0, 0, 0]];
+    charmapLT['(']   = [[0x2A, 0, 0, 0], [0x0A, 0, 0, 0], [0x8A, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['“']   = [[0x0B, 0, 0, 0], [0x8B, 0, 0, 0]];
+    charmapLT[')']   = [[0x2A, 0, 0, 0], [0x0B, 0, 0, 0], [0x8B, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['-']   = [[0x0C, 0, 0, 0], [0x8C, 0, 0, 0]];
+    charmapLT['_']   = [[0x2A, 0, 0, 0], [0x0C, 0, 0, 0], [0x8C, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['ž']   = [[0x0D, 0, 0, 0], [0x8D, 0, 0, 0]];
+    charmapLT['Ž']   = [[0x2A, 0, 0, 0], [0x0D, 0, 0, 0], [0x8D, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['0']   = [[0x52, 0, 0, 0],[0xD2, 0, 0, 0]];
+    charmapLT['1']   = [[0x4F, 0, 0, 0],[0xCF, 0, 0, 0]];
+    charmapLT['2']   = [[0x50, 0, 0, 0],[0xD0, 0, 0, 0]];
+    charmapLT['3']   = [[0x51, 0, 0, 0],[0xD1, 0, 0, 0]];
+    charmapLT['4']   = [[0x4B, 0, 0, 0],[0xCB, 0, 0, 0]];
+    charmapLT['5']   = [[0x4C, 0, 0, 0],[0xCC, 0, 0, 0]];
+    charmapLT['6']   = [[0x4D, 0, 0, 0],[0xCD, 0, 0, 0]];
+    charmapLT['7']   = [[0x47, 0, 0, 0],[0xC7, 0, 0, 0]];
+    charmapLT['8']   = [[0x48, 0, 0, 0],[0xC8, 0, 0, 0]];
+    charmapLT['9']   = [[0x49, 0, 0, 0],[0xC9, 0, 0, 0]];
+    charmapLT['q']   = [[0x10, 0, 0, 0], [0x90, 0, 0, 0]];
+    charmapLT['Q']   = [[0x2A, 0, 0, 0], [0x10, 0, 0, 0], [0x90, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['w']   = [[0x11, 0, 0, 0], [0x91, 0, 0, 0]];
+    charmapLT['W']   = [[0x2A, 0, 0, 0], [0x11, 0, 0, 0], [0x91, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['e']   = [[0x12, 0, 0, 0], [0x92, 0, 0, 0]];
+    charmapLT['E']   = [[0x2A, 0, 0, 0], [0x12, 0, 0, 0], [0x92, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['r']   = [[0x13, 0, 0, 0], [0x93, 0, 0, 0]];
+    charmapLT['R']   = [[0x2A, 0, 0, 0], [0x13, 0, 0, 0], [0x93, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['t']   = [[0x14, 0, 0, 0], [0x94, 0, 0, 0]];
+    charmapLT['T']   = [[0x2A, 0, 0, 0], [0x14, 0, 0, 0], [0x94, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['y']   = [[0x15, 0, 0, 0], [0x95, 0, 0, 0]];
+    charmapLT['Y']   = [[0x2A, 0, 0, 0], [0x15, 0, 0, 0], [0x95, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['u']   = [[0x16, 0, 0, 0], [0x96, 0, 0, 0]];
+    charmapLT['U']   = [[0x2A, 0, 0, 0], [0x16, 0, 0, 0], [0x96, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['i']   = [[0x17, 0, 0, 0], [0x97, 0, 0, 0]];
+    charmapLT['I']   = [[0x2A, 0, 0, 0], [0x17, 0, 0, 0], [0x97, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['o']   = [[0x18, 0, 0, 0], [0x98, 0, 0, 0]];
+    charmapLT['O']   = [[0x2A, 0, 0, 0], [0x18, 0, 0, 0], [0x98, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['p']   = [[0x19, 0, 0, 0], [0x99, 0, 0, 0]];
+    charmapLT['P']   = [[0x2A, 0, 0, 0], [0x19, 0, 0, 0], [0x99, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['[']   = [[0x1A, 0, 0, 0], [0x9A, 0, 0, 0]];
+    charmapLT['{']   = [[0x2A, 0, 0, 0], [0x1A, 0, 0, 0], [0x9A, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT[']']   = [[0x1B, 0, 0, 0], [0x9B, 0, 0, 0]];
+    charmapLT['}']   = [[0x2A, 0, 0, 0], [0x1B, 0, 0, 0], [0x9B, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['\\']   = [[0x2B, 0, 0, 0], [0xAB, 0, 0, 0]];
+    charmapLT['|']   = [[0x2A, 0, 0, 0], [0x2B, 0, 0, 0], [0xAB, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['a']   = [[0x1E, 0, 0, 0], [0x9E, 0, 0, 0]];
+    charmapLT['A']   = [[0x2A, 0, 0, 0], [0x1E, 0, 0, 0], [0x9E, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['s']   = [[0x1F, 0, 0, 0], [0x9F, 0, 0, 0]];
+    charmapLT['S']   = [[0x2A, 0, 0, 0], [0x1F, 0, 0, 0], [0x9F, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['d']   = [[0x20, 0, 0, 0], [0xA0, 0, 0, 0]];
+    charmapLT['D']   = [[0x2A, 0, 0, 0], [0x20, 0, 0, 0], [0xA0, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['f']   = [[0x21, 0, 0, 0], [0xA1, 0, 0, 0]];
+    charmapLT['F']   = [[0x2A, 0, 0, 0], [0x21, 0, 0, 0], [0xA1, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['g']   = [[0x22, 0, 0, 0], [0xA2, 0, 0, 0]];
+    charmapLT['G']   = [[0x2A, 0, 0, 0], [0x22, 0, 0, 0], [0xA2, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['h']   = [[0x23, 0, 0, 0], [0xA3, 0, 0, 0]];
+    charmapLT['H']   = [[0x2A, 0, 0, 0], [0x23, 0, 0, 0], [0xA3, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['j']   = [[0x24, 0, 0, 0], [0xA4, 0, 0, 0]];
+    charmapLT['J']   = [[0x2A, 0, 0, 0], [0x24, 0, 0, 0], [0xA4, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['k']   = [[0x25, 0, 0, 0], [0xA5, 0, 0, 0]];
+    charmapLT['K']   = [[0x2A, 0, 0, 0], [0x25, 0, 0, 0], [0xA5, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['l']   = [[0x26, 0, 0, 0], [0xA6, 0, 0, 0]];
+    charmapLT['L']   = [[0x2A, 0, 0, 0], [0x26, 0, 0, 0], [0xA6, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT[';']   = [[0x27, 0, 0, 0], [0xA7, 0, 0, 0]];
+    charmapLT[':']   = [[0x2A, 0, 0, 0], [0x27, 0, 0, 0], [0xA7, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['\'']  = [[0x28, 0, 0, 0], [0xA8, 0, 0, 0]];
+    charmapLT['"']   = [[0x2A, 0, 0, 0], [0x28, 0, 0, 0], [0xA8, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['z']   = [[0x2C, 0, 0, 0], [0xAC, 0, 0, 0]];
+    charmapLT['Z']   = [[0x2A, 0, 0, 0], [0x2C, 0, 0, 0], [0xAC, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['x']   = [[0x2D, 0, 0, 0], [0xAD, 0, 0, 0]];
+    charmapLT['X']   = [[0x2A, 0, 0, 0], [0x2D, 0, 0, 0], [0xAD, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['c']   = [[0x2E, 0, 0, 0], [0xAE, 0, 0, 0]];
+    charmapLT['C']   = [[0x2A, 0, 0, 0], [0x2E, 0, 0, 0], [0xAE, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['v']   = [[0x2F, 0, 0, 0], [0xAF, 0, 0, 0]];
+    charmapLT['V']   = [[0x2A, 0, 0, 0], [0x2F, 0, 0, 0], [0xAF, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['b']   = [[0x30, 0, 0, 0], [0xB0, 0, 0, 0]];
+    charmapLT['B']   = [[0x2A, 0, 0, 0], [0x30, 0, 0, 0], [0xB0, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['n']   = [[0x31, 0, 0, 0], [0xB1, 0, 0, 0]];
+    charmapLT['N']   = [[0x2A, 0, 0, 0], [0x31, 0, 0, 0], [0xB1, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['m']   = [[0x32, 0, 0, 0], [0xB2, 0, 0, 0]];
+    charmapLT['M']   = [[0x2A, 0, 0, 0], [0x32, 0, 0, 0], [0xB2, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT[',']   = [[0x33, 0, 0, 0], [0xB3, 0, 0, 0]];
+    charmapLT['<']   = [[0x2A, 0, 0, 0], [0x33, 0, 0, 0], [0xB3, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['.']   = [[0x34, 0, 0, 0], [0xB4, 0, 0, 0]];
+    charmapLT['>']   = [[0x2A, 0, 0, 0], [0x34, 0, 0, 0], [0xB4, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT['/']   = [[0x35, 0, 0, 0], [0xB5, 0, 0, 0]];
+    charmapLT['?']   = [[0x2A, 0, 0, 0], [0x35, 0, 0, 0], [0xB5, 0, 0, 0], [0xAA, 0, 0, 0]];
+    charmapLT[' ']   = [[0x39, 0, 0, 0], [0xb9, 0, 0, 0]];
+
+    var keymapLT = [];
+
+    keymapLT[27]                 = 0x1; // ESC
+    keymapLT[9]                 = 0x0F; // TAB
+    //keymapLT[20]                = 0x3A; // // BLOQ.MAY. => see the charmap, all the capital letters and shift chars send a shift in their sequence
+    keymapLT[16]                = 0x2A; // LEFT SHIFT and RIGHT SHIFT
+	keymapLT[91]                = 0x1D; // LEFT GUI (META, COMMAND) BINDED TO CONTROL
+	keymapLT[17]                = 0x1D; // LEFT CONTROL and RIGHT CONTROL
+    keymapLT[32]                = 0x39; // SPACE
+    keymapLT[8]                 = 0x0E; // BACKSPACE
+    keymapLT[13]                = 0x1C; // ENTER
+    //keymapLT[0]                 = 0x38; // RIGHT ALT (ALT GR)
+    //keymapLT[92]                = 0x5C; // RIGHT GUI (WINDOWS)
+    keymapLT[38]                = 0x48; // UP ARROW
+    keymapLT[37]                = 0x4B; // LEFT ARROW
+    keymapLT[40]                = 0x50; // DOWN ARROW
+    keymapLT[39]                = 0x4D; // RIGHT ARROW
+    keymapLT[45]                = 0x52; // INSERT
+    keymapLT[46]                = 0x53; // DELETE
+    keymapLT[36]                = 0x47; // HOME
+    keymapLT[35]                = 0x4F; // FIN
+    keymapLT[33]                = 0x49; // PAGE UP
+    keymapLT[34]                = 0x51; // PAGE UP
+    keymapLT[144]               = 0x45; // BLOQ.NUM.
+    keymapLT[145]                = 0x46; // SCROLL LOCK
+    keymapLT[112]                = 0x3B; // F1
+    keymapLT[113]                = 0x3C; // F2
+    keymapLT[114]                = 0x3D; // F3
+    keymapLT[115]                = 0x3E; // F4
+    keymapLT[116]                = 0x3F; // F5
+    keymapLT[117]                = 0x40; // F6
+    keymapLT[118]                = 0x41; // F7
+    keymapLT[119]                = 0x42; // F8
+    keymapLT[120]                = 0x43; // F9
+    keymapLT[121]                = 0x44; // F10
+    keymapLT[122]                = 0x57; // F11
+    keymapLT[123]                = 0x58; // F12
+
+    var ctrlkeymapLT = [];
+
+    ctrlkeymapLT[65]                = 0x1E; // a
+    ctrlkeymapLT[81]                = 0x10; // q
+    ctrlkeymapLT[87]                = 0x11; // w
+    ctrlkeymapLT[69]                = 0x12; // e
+    ctrlkeymapLT[82]                = 0x13; // r
+    ctrlkeymapLT[84]                = 0x14; // t
+    ctrlkeymapLT[89]                = 0x15; // y
+    ctrlkeymapLT[85]                = 0x16; // u
+    ctrlkeymapLT[73]                = 0x17; // i
+    ctrlkeymapLT[79]                = 0x18; // o
+    ctrlkeymapLT[80]                = 0x19; // p
+    ctrlkeymapLT[65]                = 0x1E; // a
+    ctrlkeymapLT[83]                = 0x1F; // s
+    ctrlkeymapLT[68]                = 0x20; // d
+    ctrlkeymapLT[70]                = 0x21; // f
+    ctrlkeymapLT[71]                = 0x22; // g
+    ctrlkeymapLT[72]                = 0x23; // h
+    ctrlkeymapLT[74]                = 0x24; // j
+    ctrlkeymapLT[75]                = 0x25; // k
+    ctrlkeymapLT[76]                = 0x26; // l
+    ctrlkeymapLT[90]                = 0x2C; // z
+    ctrlkeymapLT[88]                = 0x2D; // x
+    ctrlkeymapLT[67]                = 0x2E; // c
+    //ctrlkeymapLT[86]                = 0x2F; // v      to enable set disableClipboard = true in run.js
+    ctrlkeymapLT[66]                = 0x30; // b
+    ctrlkeymapLT[78]                = 0x31; // n
+    ctrlkeymapLT[77]                = 0x32; // m
+
+    var reservedCtrlKeymap = [];
+    reservedCtrlKeymap[86] = 0x2F; //v
+
+
+    return {
+        getKeymap: function() {
+            return keymapLT;
+        },
+
+        getCtrlKeymap: function() {
+            return ctrlkeymapLT;
+        },
+
+        getReservedCtrlKeymap: function() {
+            return reservedCtrlKeymap;
+        },
+
+        getCharmap: function() {
+            return charmapLT;
+        },
+
+        setCtrlKey: function (key, val) {
+            ctrlkeymapLT[key] = val;
+        }
+    };
+}( );
