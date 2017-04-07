600d05435feca896eccdf6b51b05b5a2d52e347a
Tadas Ustinavičius <tadas@ring.lt>
2017-04-07 14:31:27 +0300

    OpenStack: chreatet basic broker routine to refresh and create ephemeral VDI machines

--- OpenStack/broker/VMBuilder.py
+++ OpenStack/broker/VMBuilder.py
@@ -3,6 +3,7 @@ import logging
 import requests
 import time
 import Variables
+import json
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
@@ -20,7 +21,19 @@ class VMBuilder(threading.Thread):
     def stopped(self):
         return self._stop.isSet()
     def run(self):
+        logger = logging.getLogger('kvm-vdi-broker')
         if self.ephemeral_osInstanceId: #if old machine exists, delete it
-            self.http_session.post(Variables.dashboard_path + "inc/infrastructure/OpenStack/DeleteVM.php", data={'osInstanceId': self.ephemeral_osInstanceId}, verify=False, headers=Variables.http_headers)
+            reply = json.loads(self.http_session.post(Variables.dashboard_path + "inc/infrastructure/OpenStack/DeleteVM.php", data = {'vm_id': self.ephemeral_osInstanceId, 'broker':'true'}, verify=False, headers=Variables.http_headers))
+            logger.debug("Deleting VM id: %s name: %s", self.vm_id, self.vm_name);
+            if reply['delete'] == 'success':
+                print ("success");
+        logger.debug("Creating volume name: %s", self.vm_name);
+        reply = json.loads(self.http_session.post(Variables.dashboard_path + "inc/infrastructure/OpenStack/CreateVolume.php", data = {'source': self.osInstanceId, 'vm_name': self.vm_name, 'vm_type' : 'ephemeralvdi'}, verify=False, headers=Variables.http_headers).text)
+        while reply['volume']['status'] != 'available':
+            reply = json.loads(self.http_session.post(Variables.dashboard_path + "inc/infrastructure/OpenStack/GetVolumeInfo.php", data = {'volume_id': reply['volume']['id']}, verify=False, headers=Variables.http_headers).text)
+            time.sleep(2)
+            print (reply['volume']['status'])
+        logger.debug("Creating VM from id: %s with name: %s", self.osInstanceId, self.vm_name);
+        reply = self.http_session.post(Variables.dashboard_path + "inc/infrastructure/OpenStack/CreateEphemeralVM.php", data = {'vm_name': self.vm_name, 'vm_type': 'ephemeralvdi', 'volume_id': reply['volume']['id'], 'source_vm': self.osInstanceId, 'target_vm': self.ephemeral_osInstanceId}, verify=False, headers=Variables.http_headers).text
         print (self.vm_name)
         print (Variables.vms_to_build)
---
+++ inc/infrastructure/OpenStack/CreateEphemeralVM.php
@@ -0,0 +1,41 @@
+<?php
+include dirname(__FILE__) . '/../../../functions/config.php';
+require_once(dirname(__FILE__) . '/../../../functions/functions.php');
+if (!check_session()){
+    echo json_encode(array('error' => 'nologin'));
+    exit;
+}
+slash_vars();
+$vm_name = $_POST['vm_name'];
+$vm_type = $_POST['vm_type'];
+$volume_id = $_POST['volume_id'];
+$source_vm = $_POST['source_vm'];
+$target_vm = $_POST['target_vm'];
+$source_vm_info = json_decode(getVMInfo($source_vm), TRUE);
+$flavor = $source_vm_info['server']['flavor']['id'];
+$source_vm_info = getSQLArray("SELECT * FROM vms WHERE osInstanceId = '$source_vm'");
+$network_array = json_decode($source_vm_info[0]['osInstanceNetworks']);
+if (!empty($vm_name) && !empty($vm_type) && !empty($volume_id) && !empty($source_vm)){
+    $delete_on_termination = true;
+    $reply = createVM($vm_name, $flavor, $volume_id, $network_array, $delete_on_termination);
+    $result = json_decode($reply, TRUE);
+    if ($result['server']['id']){
+        $osInstanceNetworks = json_encode($network_array);
+        if (!empty($target_vm))
+            $target_vm_info = getSQLArray("SELECT osInstanceId FROM vms WHERE osInstanceId='$target_vm'");
+        if (empty($target_vm_info[0]['osInstanceId']))//if this is first time ephemeral machine is created
+            add_SQL_line("INSERT INTO vms (name, machine_type, source_volume, state, os_type, osInstanceId, osInstanceMasterVolume) VALUES ('$vm_name', '$vm_type', '{$source_vm[0]['id']}', 'building', '$os_type', '{$result['server']['id']}', '$volume_id')");
+        else
+            add_SQL_line("UPDATE vms SET clientid = '0', lastused = '0', osInstanceId = '{$result['server']['id']}' WHERE osInstanceId = '$target_vm'");
+/*        $vmArray = getSQLArray("SELECT vms.id, vms.name, vms.machine_type, vms.source_volume, vms.snapshot, vms.maintenance, vms.filecopy, vms.state,
+        vms.spice_password, vms.clientid, vms.lastused, vms.os_type, vms.locked, vms.osHypervisorName, vms.osInstanceName, vms.osInstanceId, vms2.name
+        AS source_volume_machine FROM `vms` vms left JOIN vms vms2 ON vms.source_volume=vms2.id WHERE vms.osInstanceId='{$result['server']['id']}' ORDER BY vms.name");
+        echo json_encode($vmArray[0]);
+*/
+        json_encode(array('status' => 'success'));
+    }
+    else echo $reply;
+
+}
+else
+    echo json_encode(array('error' => 'missing-values'));
