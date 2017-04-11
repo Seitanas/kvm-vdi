34dbea8aab01567933b0583acfe22cee70e900a1
Tadas Ustinavičius <tadas@ring.lt>
2017-04-11 10:23:44 +0300

    OpenStack: implemented epheremal VM rebuild routine

--- inc/infrastructure/OpenStack/BrokerLogin.php
+++ inc/infrastructure/OpenStack/BrokerLogin.php
@@ -5,6 +5,7 @@ $password = $_POST['password'];
 if ($password == $backend_pass){
     session_start();
     $_SESSION['logged']='yes';
+    OpenStackConnect();
     echo json_encode(array('login' => 'success'));
 }
 else 
