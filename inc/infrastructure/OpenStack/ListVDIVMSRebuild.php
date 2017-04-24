def81a7446b2ce5791c24d788c5491f3cde459de
Tadas Ustinavičius <tadas@ring.lt>
2017-04-24 11:16:42 +0300

    OpenStack: fixing issue, where some ephemeral VMs were not rebuilt

--- inc/infrastructure/OpenStack/ListVDIVMSRebuild.php
+++ inc/infrastructure/OpenStack/ListVDIVMSRebuild.php
@@ -9,7 +9,7 @@ slash_vars();
 $vmArray = getSQLarray("SELECT vms.id, vms.name, vms.maintenance, vms.filecopy, vms.state,
 vms.spice_password, vms.os_type, vms.locked, vms.osHypervisorName, vms.osInstanceName, vms.osInstanceId, vms2.name
 AS ephemeral_machine_name, vms2.clientid, vms2.osInstanceId AS ephemeral_osInstanceId, vms2.lastused FROM `vms` vms left JOIN vms vms2 ON vms.id=vms2.source_volume WHERE vms.machine_type = 'vdimachine' AND vms.maintenance != 'true'
-AND (vms2.osInstanceId IS NULL OR (vms2.lastused < DATE_SUB(NOW(), INTERVAL '$return_to_pool_after' MINUTE) AND vms2.clientid != '0')) ORDER BY vms.name");
+AND (vms2.osInstanceId IS NULL OR (vms2.lastused < DATE_SUB(NOW(), INTERVAL '$return_to_pool_after' MINUTE) AND vms2.lastused != '0')) ORDER BY vms.name");
 
 
 #print_r($vmArray);
